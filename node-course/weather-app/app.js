50ac76513f319e89732f796ddf96159feef89314
Pablo Melchi <pablo.melchi.dev@gmail.com>
2019-12-16 20:01:14 +0000

    Request and parse

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -2,6 +2,8 @@ const request = require ('request')
 
 const url = 'https://api.darksky.net/forecast/a7de71a1ff82c94dd69a5bf05a025729/37.8267,-122.4233'
 
-request ({url: url}, ()=> {
-    
+request ({url: url}, (error, response)=> {
+    const data = JSON.parse(response.body)
+
+    console.log(data)
 })
