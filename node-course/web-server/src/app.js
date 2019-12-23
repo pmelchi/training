093b9f784263f2503daaa1c9391e6b123d6c229d
Pablo Melchi <pablo.melchi@dev.null>
2019-12-23 20:57:44 +0000

    Added css and js

--- node-course/web-server/src/app.js
+++ node-course/web-server/src/app.js
@@ -1,10 +1,10 @@
+const path = require('path')
 const express = require ('express')
 
 const app = express()
 
-app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>')
-})
+const publicDir = path.join(__dirname, '../public')
+app.use(express.static(publicDir))
 
 app.get('/help', (req, res) => {
     res.send([{
